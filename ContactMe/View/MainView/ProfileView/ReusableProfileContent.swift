8f81980ce5c61a117ec9281f18abe2951cbd19d3
Dom Montalto <dommontalto@outlook.com>
2023-05-03 19:11:31 +0100

    Added profile usernames

--- ContactMe/View/MainView/ProfileView/ReusableProfileContent.swift
+++ ContactMe/View/MainView/ProfileView/ReusableProfileContent.swift
@@ -13,10 +13,10 @@ struct ReusableProfileContent: View {
    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            LazyVStack{
-                HStack(spacing: 12){
-                    WebImage(url: user.userProfileURL).placeholder{
-                        // MARK: Placeholder Imgae
+            LazyVStack(alignment: .leading) {
+                HStack(spacing: 12) {
+                    WebImage(url: user.userProfileURL).placeholder {
+                        // MARK: Placeholder Image
                         Image("NullProfile")
                             .resizable()
                     }
@@ -34,13 +34,50 @@ struct ReusableProfileContent: View {
                             .font(.caption)
                             .foregroundColor(.gray)
                             .lineLimit(3)
-                        
                     }
-                    .hAlign(.leading)
                 }
-                
+
+                // Add the icons and text here
+                VStack(alignment: .leading, spacing: 8) {
+                    HStack(spacing: 12) {
+                        Image("Mobile") // Replace with your mobile image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                    HStack(spacing: 12) {
+                        Image("Email") // Replace with your email image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                    HStack(spacing: 12) {
+                        Image("Twitter") // Replace with your Twitter image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                    HStack(spacing: 12) {
+                        Image("Instagram") // Replace with your Instagram image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                    HStack(spacing: 12) {
+                        Image("Telegram") // Replace with your Telegram image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.top, 8)
             }
-            .padding(15)
+            .padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
         }
     }
 }
